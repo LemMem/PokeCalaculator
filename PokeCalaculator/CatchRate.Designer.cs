598e7ddef5bbd4803d62ea0538cd5453aff77f31
catmaster450 <yoshi3lm201@gmail.com>
2019-04-07 15:43:34 -0400

    Changes to CatchRate
    -Redid catch rate
    
    Also no, I will not include gen 1 or 2. Sorry.

--- PokeCalaculator/CatchRate.Designer.cs
+++ PokeCalaculator/CatchRate.Designer.cs
@@ -29,30 +29,12 @@
         private void InitializeComponent()
         {
             this.descLabel = new System.Windows.Forms.Label();
-            this.OpponentsMaxHPG34 = new System.Windows.Forms.TextBox();
-            this.maxHpLabelG34 = new System.Windows.Forms.Label();
-            this.CurrentHpLabelG34 = new System.Windows.Forms.Label();
-            this.OpponentsCurrentHpG34 = new System.Windows.Forms.TextBox();
-            this.OpponentsCatchRateLabelG34 = new System.Windows.Forms.LinkLabel();
-            this.OpponentsCatchRateG34 = new System.Windows.Forms.TextBox();
-            this.PokeballG34 = new System.Windows.Forms.TextBox();
             this.rand0 = new System.Windows.Forms.Label();
-            this.StatusValG34 = new System.Windows.Forms.Label();
-            this.StatG34 = new System.Windows.Forms.TextBox();
-            this.calcG34 = new System.Windows.Forms.Button();
-            this.G34 = new System.Windows.Forms.Panel();
-            this.PokeballMultiplierG34 = new System.Windows.Forms.LinkLabel();
-            this.descStatVal2G34 = new System.Windows.Forms.Label();
-            this.descStatValG34 = new System.Windows.Forms.Label();
-            this.G34Label = new System.Windows.Forms.Label();
             this.resultText = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.G1 = new System.Windows.Forms.Panel();
-            this.descLabelG1 = new System.Windows.Forms.Label();
-            this.PokeballUsedLabelG1 = new System.Windows.Forms.Label();
-            this.MaxHP = new System.Windows.Forms.TextBox();
-            this.G34.SuspendLayout();
-            this.G1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Generation34Radio = new System.Windows.Forms.RadioButton();
+            this.Generation5radio = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // descLabel
@@ -64,64 +46,6 @@
             this.descLabel.TabIndex = 0;
             this.descLabel.Text = "This will show you the chance you have to catch a pokemon.";
             // 
-            // OpponentsMaxHPG34
-            // 
-            this.OpponentsMaxHPG34.Location = new System.Drawing.Point(7, 49);
-            this.OpponentsMaxHPG34.Name = "OpponentsMaxHPG34";
-            this.OpponentsMaxHPG34.Size = new System.Drawing.Size(100, 20);
-            this.OpponentsMaxHPG34.TabIndex = 1;
-            // 
-            // maxHpLabelG34
-            // 
-            this.maxHpLabelG34.AutoSize = true;
-            this.maxHpLabelG34.Location = new System.Drawing.Point(4, 33);
-            this.maxHpLabelG34.Name = "maxHpLabelG34";
-            this.maxHpLabelG34.Size = new System.Drawing.Size(99, 13);
-            this.maxHpLabelG34.TabIndex = 2;
-            this.maxHpLabelG34.Text = "Opponents max hp:";
-            // 
-            // CurrentHpLabelG34
-            // 
-            this.CurrentHpLabelG34.AutoSize = true;
-            this.CurrentHpLabelG34.Location = new System.Drawing.Point(3, 72);
-            this.CurrentHpLabelG34.Name = "CurrentHpLabelG34";
-            this.CurrentHpLabelG34.Size = new System.Drawing.Size(113, 13);
-            this.CurrentHpLabelG34.TabIndex = 3;
-            this.CurrentHpLabelG34.Text = "Opponents current hp:";
-            // 
-            // OpponentsCurrentHpG34
-            // 
-            this.OpponentsCurrentHpG34.Location = new System.Drawing.Point(7, 88);
-            this.OpponentsCurrentHpG34.Name = "OpponentsCurrentHpG34";
-            this.OpponentsCurrentHpG34.Size = new System.Drawing.Size(100, 20);
-            this.OpponentsCurrentHpG34.TabIndex = 4;
-            // 
-            // OpponentsCatchRateLabelG34
-            // 
-            this.OpponentsCatchRateLabelG34.AutoSize = true;
-            this.OpponentsCatchRateLabelG34.Location = new System.Drawing.Point(3, 111);
-            this.OpponentsCatchRateLabelG34.Name = "OpponentsCatchRateLabelG34";
-            this.OpponentsCatchRateLabelG34.Size = new System.Drawing.Size(113, 13);
-            this.OpponentsCatchRateLabelG34.TabIndex = 5;
-            this.OpponentsCatchRateLabelG34.TabStop = true;
-            this.OpponentsCatchRateLabelG34.Text = "Opponents catch rate:";
-            this.OpponentsCatchRateLabelG34.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // OpponentsCatchRateG34
-            // 
-            this.OpponentsCatchRateG34.Location = new System.Drawing.Point(6, 127);
-            this.OpponentsCatchRateG34.Name = "OpponentsCatchRateG34";
-            this.OpponentsCatchRateG34.Size = new System.Drawing.Size(100, 20);
-            this.OpponentsCatchRateG34.TabIndex = 6;
-            // 
-            // PokeballG34
-            // 
-            this.PokeballG34.Location = new System.Drawing.Point(6, 166);
-            this.PokeballG34.Name = "PokeballG34";
-            this.PokeballG34.Size = new System.Drawing.Size(100, 20);
-            this.PokeballG34.TabIndex = 8;
-            this.PokeballG34.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
-            // 
             // rand0
             // 
             this.rand0.AutoSize = true;
@@ -129,92 +53,6 @@
             this.rand0.Name = "rand0";
             this.rand0.Size = new System.Drawing.Size(0, 13);
             this.rand0.TabIndex = 9;
-            // 
-            // StatusValG34
-            // 
-            this.StatusValG34.AutoSize = true;
-            this.StatusValG34.Location = new System.Drawing.Point(3, 189);
-            this.StatusValG34.Name = "StatusValG34";
-            this.StatusValG34.Size = new System.Drawing.Size(70, 13);
-            this.StatusValG34.TabIndex = 10;
-            this.StatusValG34.Text = "Status Value:";
-            // 
-            // StatG34
-            // 
-            this.StatG34.Location = new System.Drawing.Point(6, 205);
-            this.StatG34.Name = "StatG34";
-            this.StatG34.Size = new System.Drawing.Size(100, 20);
-            this.StatG34.TabIndex = 11;
-            this.StatG34.TextChanged += new System.EventHandler(this.StatG34_TextChanged);
-            // 
-            // calcG34
-            // 
-            this.calcG34.Location = new System.Drawing.Point(3, 257);
-            this.calcG34.Name = "calcG34";
-            this.calcG34.Size = new System.Drawing.Size(167, 23);
-            this.calcG34.TabIndex = 13;
-            this.calcG34.Text = "Calculate";
-            this.calcG34.UseVisualStyleBackColor = true;
-            this.calcG34.Click += new System.EventHandler(this.calcG34_Click);
-            // 
-            // G34
-            // 
-            this.G34.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.G34.Controls.Add(this.PokeballMultiplierG34);
-            this.G34.Controls.Add(this.descStatVal2G34);
-            this.G34.Controls.Add(this.calcG34);
-            this.G34.Controls.Add(this.descStatValG34);
-            this.G34.Controls.Add(this.maxHpLabelG34);
-            this.G34.Controls.Add(this.OpponentsMaxHPG34);
-            this.G34.Controls.Add(this.CurrentHpLabelG34);
-            this.G34.Controls.Add(this.StatG34);
-            this.G34.Controls.Add(this.OpponentsCurrentHpG34);
-            this.G34.Controls.Add(this.StatusValG34);
-            this.G34.Controls.Add(this.OpponentsCatchRateLabelG34);
-            this.G34.Controls.Add(this.OpponentsCatchRateG34);
-            this.G34.Controls.Add(this.PokeballG34);
-            this.G34.Location = new System.Drawing.Point(18, 65);
-            this.G34.Name = "G34";
-            this.G34.Size = new System.Drawing.Size(178, 292);
-            this.G34.TabIndex = 14;
-            // 
-            // PokeballMultiplierG34
-            // 
-            this.PokeballMultiplierG34.AutoSize = true;
-            this.PokeballMultiplierG34.Location = new System.Drawing.Point(5, 150);
-            this.PokeballMultiplierG34.Name = "PokeballMultiplierG34";
-            this.PokeballMultiplierG34.Size = new System.Drawing.Size(102, 13);
-            this.PokeballMultiplierG34.TabIndex = 14;
-            this.PokeballMultiplierG34.TabStop = true;
-            this.PokeballMultiplierG34.Text = "Pokeball\'s Multiplier:";
-            this.PokeballMultiplierG34.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
-            // 
-            // descStatVal2G34
-            // 
-            this.descStatVal2G34.AutoSize = true;
-            this.descStatVal2G34.Location = new System.Drawing.Point(4, 241);
-            this.descStatVal2G34.Name = "descStatVal2G34";
-            this.descStatVal2G34.Size = new System.Drawing.Size(161, 13);
-            this.descStatVal2G34.TabIndex = 13;
-            this.descStatVal2G34.Text = "poison, or burn, and 1 otherwise)";
-            // 
-            // descStatValG34
-            // 
-            this.descStatValG34.AutoSize = true;
-            this.descStatValG34.Location = new System.Drawing.Point(3, 228);
-            this.descStatValG34.Name = "descStatValG34";
-            this.descStatValG34.Size = new System.Drawing.Size(140, 13);
-            this.descStatValG34.TabIndex = 12;
-            this.descStatValG34.Text = "(2 for sleep, 1.5 for paralyze,";
-            // 
-            // G34Label
-            // 
-            this.G34Label.AutoSize = true;
-            this.G34Label.Location = new System.Drawing.Point(21, 46);
-            this.G34Label.Name = "G34Label";
-            this.G34Label.Size = new System.Drawing.Size(61, 13);
-            this.G34Label.TabIndex = 15;
-            this.G34Label.Text = "Gen III - IV:";
             // 
             // resultText
             // 
@@ -234,61 +72,52 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // G1
+            // label1
             // 
-            this.G1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.G1.Controls.Add(this.MaxHP);
-            this.G1.Controls.Add(this.PokeballUsedLabelG1);
-            this.G1.Location = new System.Drawing.Point(203, 65);
-            this.G1.Name = "G1";
-            this.G1.Size = new System.Drawing.Size(178, 292);
-            this.G1.TabIndex = 18;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Generation:";
             // 
-            // descLabelG1
+            // Generation34Radio
             // 
-            this.descLabelG1.AutoSize = true;
-            this.descLabelG1.Location = new System.Drawing.Point(203, 46);
-            this.descLabelG1.Name = "descLabelG1";
-            this.descLabelG1.Size = new System.Drawing.Size(36, 13);
-            this.descLabelG1.TabIndex = 19;
-            this.descLabelG1.Text = "Gen I:";
+            this.Generation34Radio.AutoSize = true;
+            this.Generation34Radio.Location = new System.Drawing.Point(13, 82);
+            this.Generation34Radio.Name = "Generation34Radio";
+            this.Generation34Radio.Size = new System.Drawing.Size(121, 17);
+            this.Generation34Radio.TabIndex = 19;
+            this.Generation34Radio.TabStop = true;
+            this.Generation34Radio.Text = "Generations 3 and 4";
+            this.Generation34Radio.UseVisualStyleBackColor = true;
             // 
-            // PokeballUsedLabelG1
+            // Generation5radio
             // 
-            this.PokeballUsedLabelG1.AutoSize = true;
-            this.PokeballUsedLabelG1.Location = new System.Drawing.Point(3, 33);
-            this.PokeballUsedLabelG1.Name = "PokeballUsedLabelG1";
-            this.PokeballUsedLabelG1.Size = new System.Drawing.Size(77, 13);
-            this.PokeballUsedLabelG1.TabIndex = 0;
-            this.PokeballUsedLabelG1.Text = "Pokeball used:";
-            // 
-            // MaxHP
-            // 
-            this.MaxHP.Location = new System.Drawing.Point(6, 48);
-            this.MaxHP.Name = "MaxHP";
-            this.MaxHP.Size = new System.Drawing.Size(100, 20);
-            this.MaxHP.TabIndex = 1;
+            this.Generation5radio.AutoSize = true;
+            this.Generation5radio.Location = new System.Drawing.Point(13, 106);
+            this.Generation5radio.Name = "Generation5radio";
+            this.Generation5radio.Size = new System.Drawing.Size(86, 17);
+            this.Generation5radio.TabIndex = 20;
+            this.Generation5radio.TabStop = true;
+            this.Generation5radio.Text = "Generation 5";
+            this.Generation5radio.UseVisualStyleBackColor = true;
             // 
             // CatchRate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.descLabelG1);
-            this.Controls.Add(this.G1);
+            this.ClientSize = new System.Drawing.Size(406, 450);
+            this.Controls.Add(this.Generation5radio);
+            this.Controls.Add(this.Generation34Radio);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.resultText);
-            this.Controls.Add(this.G34Label);
-            this.Controls.Add(this.G34);
             this.Controls.Add(this.rand0);
             this.Controls.Add(this.descLabel);
             this.Name = "CatchRate";
             this.Text = "Opponents current hp:";
-            this.G34.ResumeLayout(false);
-            this.G34.PerformLayout();
-            this.G1.ResumeLayout(false);
-            this.G1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -297,27 +126,11 @@
         #endregion
 
         private System.Windows.Forms.Label descLabel;
-        private System.Windows.Forms.TextBox OpponentsMaxHPG34;
-        private System.Windows.Forms.Label maxHpLabelG34;
-        private System.Windows.Forms.Label CurrentHpLabelG34;
-        private System.Windows.Forms.TextBox OpponentsCurrentHpG34;
-        private System.Windows.Forms.LinkLabel OpponentsCatchRateLabelG34;
-        private System.Windows.Forms.TextBox OpponentsCatchRateG34;
-        private System.Windows.Forms.TextBox PokeballG34;
         private System.Windows.Forms.Label rand0;
-        private System.Windows.Forms.Label StatusValG34;
-        private System.Windows.Forms.TextBox StatG34;
-        private System.Windows.Forms.Button calcG34;
-        private System.Windows.Forms.Panel G34;
-        private System.Windows.Forms.Label descStatVal2G34;
-        private System.Windows.Forms.Label descStatValG34;
-        private System.Windows.Forms.Label G34Label;
         private System.Windows.Forms.Label resultText;
-        private System.Windows.Forms.LinkLabel PokeballMultiplierG34;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel G1;
-        private System.Windows.Forms.Label descLabelG1;
-        private System.Windows.Forms.TextBox MaxHP;
-        private System.Windows.Forms.Label PokeballUsedLabelG1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton Generation34Radio;
+        private System.Windows.Forms.RadioButton Generation5radio;
     }
 }
--- PokeCalaculator/CatchRate.cs
+++ PokeCalaculator/CatchRate.cs
@@ -17,44 +17,10 @@ namespace PokeCalaculator
             InitializeComponent();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            this.OpponentsCatchRateLabelG34.LinkVisited = true;
-
-            System.Diagnostics.Process.Start("https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_catch_rate");
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void calcG34_Click(object sender, EventArgs e)
+        
+        void calculateCatchRate(float generation, float maxHp, float currentHp, float rate, float bonusBall, float bonusStatus)
         {
             
-            try
-            {
-                float result = 0;
-                float c = (3 * float.Parse(OpponentsMaxHPG34.Text) - 2 * float.Parse(OpponentsCurrentHpG34.Text)) * float.Parse(OpponentsCatchRateG34.Text) * float.Parse(PokeballG34.Text);
-                float d = 3 * float.Parse(OpponentsMaxHPG34.Text);
-                result = c / d * float.Parse(StatG34.Text);
-                resultText.Text = "Result: " + result.ToString() + "%!";
-            } catch(FormatException)
-            {
-                resultText.Text = "Error: That's not a number!";
-            }
-        }
-
-        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            this.PokeballMultiplierG34.LinkVisited = true;
-
-            System.Diagnostics.Process.Start("https://bulbapedia.bulbagarden.net/wiki/Catch_rate#Pok.C3.A9_Ball_effectiveness_rates");
-        }
-
-        private void StatG34_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
